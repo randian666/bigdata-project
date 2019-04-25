b2abc43d9ce8ffd2f3486ff8f6806081a2c965fd
553798608@qq.com <553798608@qq.com>
2019-04-25 15:13:04 +0800

    flink入门程序

--- flink-project/src/main/java/com/flink/demo/DemoScoketFlink.java
+++ flink-project/src/main/java/com/flink/demo/DemoScoketFlink.java
@@ -4,8 +4,10 @@ import com.alibaba.fastjson.JSON;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.functions.ReduceFunction;
 import org.apache.flink.api.java.tuple.Tuple;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.CheckpointingMode;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.LocalStreamEnvironment;
@@ -23,7 +25,7 @@ import java.util.Map;
 
 /**
  *
- *
+ * flink入门程序
  * @Author: liuxun
  * @CreateDate: 2018/6/18 下午2:50
  * @Version: 1.0
@@ -32,6 +34,8 @@ public class DemoScoketFlink {
     private static final Logger LOG = LoggerFactory.getLogger(DemoScoketFlink.class);
 
     public static void main(String[] args) throws Exception {
+//        ParameterTool params = ParameterTool.fromArgs(args);//解析入参
+        //获取运行时环境
       LocalStreamEnvironment env = StreamExecutionEnvironment.createLocalEnvironment();
 //        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         // 开启checkpoint机制，确保精确处理一次
@@ -59,30 +63,48 @@ public class DemoScoketFlink {
                 //每隔5秒收集数据流
                 .timeWindow(Time.seconds(5))
                 //5秒内接受到的数据进行处理
-                .apply(new RichWindowFunction<Tuple2<String, Integer>, Tuple2<String, Integer>, Tuple, TimeWindow>() {
-                    @Override
-                    public void apply(Tuple tuple, TimeWindow window, Iterable<Tuple2<String, Integer>> input, Collector<Tuple2<String, Integer>> out) throws Exception {
-                        Map<String,Integer> currMap=new HashMap();
-                        for (Tuple2<String,Integer> in:input){
-                            if (!currMap.containsKey(in.f0)){
-                                currMap.put(in.f0,0);
-                            }
-                            currMap.put(in.f0,currMap.get(in.f0)+in.f1);
-                        }
-                        for (Map.Entry entry:currMap.entrySet()){
-                            out.collect(new Tuple2<String, Integer>(entry.getKey().toString(),Integer.parseInt(entry.getValue().toString())));
-                        }
-                    }
-                });
+                //通过reduce聚合
+//                .reduce(new ReduceFunction<Tuple2<String, Integer>>() {
+//                    @Override
+//                    public Tuple2<String, Integer> reduce(Tuple2<String, Integer> a, Tuple2<String, Integer> b) throws Exception {
+//                        return new Tuple2(a.f0,a.f1+b.f1);
+//                    }
+//                });
+        //通过sum函数聚合
+                .sum(1);
+        //通过apply方法聚合
+//                .apply(new RichWindowFunction<Tuple2<String, Integer>, Tuple2<String, Integer>, Tuple, TimeWindow>() {
+//                    @Override
+//                    public void apply(Tuple tuple, TimeWindow window, Iterable<Tuple2<String, Integer>> input, Collector<Tuple2<String, Integer>> out) throws Exception {
+//                        Map<String,Integer> currMap=new HashMap();
+//                        for (Tuple2<String,Integer> in:input){
+//                            if (!currMap.containsKey(in.f0)){
+//                                currMap.put(in.f0,0);
+//                            }
+//                            currMap.put(in.f0,currMap.get(in.f0)+in.f1);
+//                        }
+//                        for (Map.Entry entry:currMap.entrySet()){
+//                            out.collect(new Tuple2<String, Integer>(entry.getKey().toString(),Integer.parseInt(entry.getValue().toString())));
+//                        }
+//                    }
+//                });
+        //数据存储
         dataStream.addSink(new RichSinkFunction<Tuple2<String, Integer>>() {
             @Override
             public void invoke(Tuple2<String, Integer> value, Context context) throws Exception {
-                System.out.println("sink result is "+JSON.toJSON(value));
+                System.out.println("sink result1 is "+JSON.toJSON(value));
+            }
+        }).name("sink result1");
+        dataStream.addSink(new RichSinkFunction<Tuple2<String, Integer>>() {
+            @Override
+            public void invoke(Tuple2<String, Integer> value, Context context) throws Exception {
+                System.out.println("sink result2 is "+JSON.toJSON(value));
             }
         })
-        .name("sink result");
+        .name("sink result2");
         dataStream.print();
         System.out.println(env.getExecutionPlan());
+        //Flink程序是延迟计算。只有执行了execute才真正触发执行。
         env.execute("window WordCount");
     }
 }
